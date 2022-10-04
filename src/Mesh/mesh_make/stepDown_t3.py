e58ffe012e10a7a67e085231c676b4a5543cf333
Konstantinos Mixios <k.mixios@gmail.com>
2022-10-04 17:36:10 +0300

    UPDATE

--- src/Mesh/mesh_make/stepDown_t3.py
+++ src/Mesh/mesh_make/stepDown_t3.py
@@ -8,4 +8,20 @@ def stepDown_t3(xlim, ylim, nx, ny):
     hy = d / (2 - 1 / 2 ** (ny - 2))
     lnods = []
     coord = []
-    for j in 
+    for j in range(ny-1):
+        if j > 0:
+            ne = len(lnods)
+            nn = len(coord) - nx
+        else:
+            ne = 0
+            nn = 0
+        lnods = [lnods, np.zeros(3*(nx-1), 3)]
+    for i in range(nx-1):
+        k = ne + 3*(i-1)
+        n1 = nn + i
+        n2 = n1 + 1
+        n3 = nn + nx +(i-1)*2 +1
+        n4 = n3 + 1
+        n5 = n4 + 1
+
+        lnods[k+1][0], lnods[k+1][1], lnods[k+1][2] = n1, n4, n3
